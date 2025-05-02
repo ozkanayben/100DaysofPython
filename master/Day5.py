b28cffe609e70ff2c3e10794daad548940fa8484
unknown <ozkannayben@gmail.com>
2025-05-01 23:25:02 -0500

    update day5

--- master/Day5.py
+++ master/Day5.py
@@ -3,3 +3,43 @@
 fruits = ["apple", "peach", "kiwi"]
 for fruit in fruits:
     print(fruit)
+
+
+    student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+print(range(1, 10)) 
+
+# range function
+
+for number in range(1,10,2):
+    print(number)
+
+
+# password generator
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list += random.choice(symbols)
+
+for char in range(0, nr_numbers):
+    password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+password = ""
+for char in password_list:
+    password += char
+    print(f"your password is: {password}")
