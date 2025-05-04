c53137362c8bcf3aa0dbaa7099c9c1123fa51351
unknown <ozkannayben@gmail.com>
2025-05-03 19:03:56 -0500

    Day7 updates

--- master/Day6.py
+++ master/Day6.py
@@ -1,6 +1,20 @@
-# Functions
+# Functions & Indentation
 
 def my_function():
     print("hellofunction")
 
     my_function () 
+
+""" For
+
+for item in list_of_items:
+    do something to each of item
+
+for number in range(a, b):
+    print(number)
+
+While 
+
+while something_is_true:
+    do something repeatedly
+"""
